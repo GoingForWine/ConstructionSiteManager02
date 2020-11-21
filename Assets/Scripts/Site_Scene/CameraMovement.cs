f93ce09ee693c7fe45c34fc1f234160552baeabf
Nathan King <38962677+nathanking2018@users.noreply.github.com>
2020-11-21 01:54:29 +0000

    Added basic camera movement
    - I added my version of the camera movement

--- Assets/Scripts/Site_Scene/CameraMovement.cs
+++ Assets/Scripts/Site_Scene/CameraMovement.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Transform[] movementPoints;
+
+    [SerializeField]
+    private float movSpeed;
+    [SerializeField]
+    private float rotSpeed;
+
+
+    private Rigidbody rigbody;
+
+    //private int desiredPoint; // This will be used another time
+    private int selectedPoint;
+
+    private bool canMoveRight;
+
+    private bool isAbleToMove; // This will be used properly later
+
+
     void Start()
     {
-        
+        rigbody = GetComponent<Rigidbody>();
+
+        isAbleToMove = true;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void FixedUpdate()
+    {
+        // Rotation (smooth rotation)
+        Quaternion rotationTarget = Quaternion.LookRotation(movementPoints[selectedPoint].position - this.transform.position);
+        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotationTarget, rotSpeed * Time.deltaTime);
+        
+
+
+        // Determine whether the camera will move to the right or the left
+        if (transform.position == movementPoints[0].position)
+        {
+            canMoveRight = true;
+        }
+        else if (transform.position == movementPoints[5].position)
+        {
+            canMoveRight = false;
+        }
+
+
+
+        // Wait until camera has rotation in the direction of the next point before moving
+        if (this.transform.rotation == rotationTarget)
+        {
+            // Move until you reach the current object/waypoint
+            if (transform.position != movementPoints[selectedPoint].position)
+            {
+                Vector3 pos = Vector3.MoveTowards(transform.position, movementPoints[selectedPoint].position, movSpeed * Time.deltaTime);
+                rigbody.MovePosition(pos);
+            }
+        }
+
+        // Once the camera has reached the current movement point, move to the next point
+        if (transform.position == movementPoints[selectedPoint].position)
+        {
+            // Move camera backwards when it reaches the last point
+            if (canMoveRight == true)
+            {
+                selectedPoint = (selectedPoint + 1) % movementPoints.Length;
+            }
+            else if (canMoveRight == false)
+            {
+                selectedPoint = (selectedPoint - 1) % movementPoints.Length;
+            }
+        }
     }
 }
