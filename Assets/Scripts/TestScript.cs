b1b34c2201a0db7057443e2c168cd612611d6c83
SeanDarrah <72384918+SeanDarrah@users.noreply.github.com>
2020-11-23 20:04:27 +0000

    Animations Working
    Got the relative animations working depending on which response the player chooses.

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,24 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestScript : MonoBehaviour
 {
     public Animator anim;
+    public Button responseButton1;
+    public Button responseButton2;
+    public Button responseButton3;
     void Start()
     {
         anim = GetComponent<Animator>();
+        Button btn1 = responseButton1.GetComponent<Button>();
+        Button btn2 = responseButton2.GetComponent<Button>();
+        Button btn3 = responseButton3.GetComponent<Button>();
+        btn1.onClick.AddListener(responseText1);
+        btn2.onClick.AddListener(responseText2);
+        btn3.onClick.AddListener(responseText3);
     }
 
-    // Update is called once per frame
-    void Update()
+    void responseText1()
     {
-        if (Input.GetKey("space"))
-        {
-            anim.SetBool("IsBool", true);
-            Debug.Log("hello");
-        }
-        else
-            anim.SetBool("IsBool", false);
+        anim.SetBool("Response1Bool", true);
+        Debug.Log("Clicked response 1");
+    }
+
+    void responseText2()
+    {
+        anim.SetBool("Response2Bool", true);
+        Debug.Log("Clicked response 2");
+    }
+
+    void responseText3()
+    {
+        anim.SetBool("Response3Bool", true);
+        Debug.Log("Clicked response 3");
     }
 }
