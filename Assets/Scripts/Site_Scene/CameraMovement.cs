499bc26e08467d5988ba58e2e4ea1295c09b0b32
Nathan King <38962677+nathanking2018@users.noreply.github.com>
2020-11-23 01:36:44 +0000

    Did stuff
    - Deleted some unnecessary images from mutliple folders
    
    - Added some basic interaction to the 3D owl character (brings up sean's choice system)
    
    - Added the 'Relationship' value to the office relationship panel

--- Assets/Scripts/Site_Scene/CameraMovement.cs
+++ Assets/Scripts/Site_Scene/CameraMovement.cs
@@ -14,12 +14,11 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     private Transform[] movementPoints;
     [SerializeField]
-    private Transform[] rotationPoints;
-
-
-    [SerializeField]
     private float movSpeed;
     private float saveMovSpeed; // Save the currently set move speed (so you just have to use 'movSpeed = saveMovSpeed', instead of 'movSpeed = (value)', it just makes things easier
+
+    [SerializeField]
+    private Transform[] rotationPoints;
     [SerializeField]
     private float rotSpeed;
 
@@ -58,67 +57,72 @@ public class CameraMovement : MonoBehaviour
         selectedRotPoint = selectedMovPoint; // Automatically set the current rotation point number, to the current movement point number
 
 
-        
-        // If the camera position is equal to the movement point position, and if the camera is rotating towards a rotation point, and if isMoving is set to false, then...
-        // Movement Point 0
-        if (transform.position == movementPoints[0].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        // If the camera is rotating towards a rotation point, and if isMoving is set to false, then...
+        if (transform.rotation == rotationTargetRotation && isMoving == false)
         {
-            disableButtonRight.SetActive(true);
+            //Debug.Log("This is being accessed!!!!!!!!!!!!");
 
-            disableOfficeMenu.SetActive(true);
+            // Movement Point 0
+            if (transform.position == movementPoints[0].position)// && transform.rotation == rotationTargetRotation && isMoving == false)
+            {
+                disableButtonRight.SetActive(true);
+
+                disableOfficeMenu.SetActive(true);
+            }
+
+
+            // Movement Point 1
+            if (transform.position == movementPoints[1].position)// && transform.rotation == rotationTargetRotation && isMoving == false)
+            {
+                disableButtonRight.SetActive(true);
+                disableButtonLeft.SetActive(true);
+            }
+
+
+            // Movement Point 2
+            if (transform.position == movementPoints[2].position)//  && transform.rotation == rotationTargetRotation && isMoving == false)
+            {
+                disableButtonRight.SetActive(true);
+                disableButtonLeft.SetActive(true);
+            }
+
+
+            // Movement Point 3
+            if (transform.position == movementPoints[3].position)//  && transform.rotation == rotationTargetRotation && isMoving == false)
+            {
+                disableButtonRight.SetActive(true);
+                disableButtonLeft.SetActive(true);
+            }
+
+
+            // Movement Point 4
+            if (transform.position == movementPoints[4].position)//  && transform.rotation == rotationTargetRotation && isMoving == false)
+            {
+                disableButtonRight.SetActive(true);
+                disableButtonLeft.SetActive(true);
+            }
+
+
+            // Movement Point 5
+            if (transform.position == movementPoints[5].position)//  && transform.rotation == rotationTargetRotation && isMoving == false)
+            {
+                disableButtonLeft.SetActive(true);
+                disableButtonRight.SetActive(true);
+            }
+
+
+            // Movement Point 6
+            if (transform.position == movementPoints[6].position)//  && transform.rotation == rotationTargetRotation && isMoving == false)
+            {
+                disableButtonLeft.SetActive(true);
+            }
         }
-        else if (transform.position != movementPoints[selectedMovPoint].position && transform.rotation != rotationTargetRotation && isMoving == true)
+        
+        
+        if (transform.position == movementPoints[0].position && transform.rotation != rotationTargetRotation && isMoving == true)
         {
             disableOfficeMenu.SetActive(false);
         }
-
-        // Movement Point 1
-        if (transform.position == movementPoints[1].position && transform.rotation == rotationTargetRotation && isMoving == false)
-        {
-            disableButtonRight.SetActive(true);
-            disableButtonLeft.SetActive(true);
-        }
-       
-
-        // Movement Point 2
-        if (transform.position == movementPoints[2].position && transform.rotation == rotationTargetRotation && isMoving == false)
-        {
-            disableButtonRight.SetActive(true);
-            disableButtonLeft.SetActive(true);
-        }
-        
-
-        // Movement Point 3
-        if (transform.position == movementPoints[3].position && transform.rotation == rotationTargetRotation && isMoving == false)
-        {
-            disableButtonRight.SetActive(true);
-            disableButtonLeft.SetActive(true);
-        }
-        
-
-        // Movement Point 4
-        if (transform.position == movementPoints[4].position && transform.rotation == rotationTargetRotation && isMoving == false)
-        {
-            disableButtonRight.SetActive(true);
-            disableButtonLeft.SetActive(true);
-        }
-        
-
-        // Movement Point 5
-        if (transform.position == movementPoints[5].position && transform.rotation == rotationTargetRotation && isMoving == false)
-        {
-            disableButtonLeft.SetActive(true);
-            disableButtonRight.SetActive(true);
-        }
-
-
-        // Movement Point 6
-        if (transform.position == movementPoints[6].position && transform.rotation == rotationTargetRotation && isMoving == false)
-        {
-            disableButtonLeft.SetActive(true);
-        }
-
-
 
 
         if (transform.position != movementPoints[selectedMovPoint].position)
@@ -126,10 +130,75 @@ public class CameraMovement : MonoBehaviour
             disableButtonRight.SetActive(false);
             disableButtonLeft.SetActive(false);
         }
-        
+
+        //// If the camera position is equal to the movement point position, and if the camera is rotating towards a rotation point, and if isMoving is set to false, then...
+        //// Movement Point 0
+        //if (transform.position == movementPoints[0].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        //{
+        //    Debug.Log("This is working!");
+
+        //    disableButtonRight.SetActive(true);
+
+        //    disableOfficeMenu.SetActive(true);
+        //}
+        //else if (transform.position != movementPoints[0].position && transform.rotation != rotationTargetRotation && isMoving == true)
+        //{
+        //    Debug.Log("This is NOT working!");
+
+        //    disableOfficeMenu.SetActive(false);
+        //}
+
+
+        //// Movement Point 1
+        //if (transform.position == movementPoints[1].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        //{
+        //    disableButtonRight.SetActive(true);
+        //    disableButtonLeft.SetActive(true);
+        //}
+
+
+        //// Movement Point 2
+        //if (transform.position == movementPoints[2].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        //{
+        //    disableButtonRight.SetActive(true);
+        //    disableButtonLeft.SetActive(true);
+        //}
+
+
+        //// Movement Point 3
+        //if (transform.position == movementPoints[3].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        //{
+        //    disableButtonRight.SetActive(true);
+        //    disableButtonLeft.SetActive(true);
+        //}
+
+
+        //// Movement Point 4
+        //if (transform.position == movementPoints[4].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        //{
+        //    disableButtonRight.SetActive(true);
+        //    disableButtonLeft.SetActive(true);
+        //}
+
+
+        //// Movement Point 5
+        //if (transform.position == movementPoints[5].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        //{
+        //    disableButtonLeft.SetActive(true);
+        //    disableButtonRight.SetActive(true);
+        //}
+
+
+        //// Movement Point 6
+        //if (transform.position == movementPoints[6].position && transform.rotation == rotationTargetRotation && isMoving == false)
+        //{
+        //    disableButtonLeft.SetActive(true);
+        //}
+
+
 
         //Debug.Log(selectedMovPoint);
-        //Debug.Log(selectedRotPoint);
+        Debug.Log(selectedRotPoint);
         //Debug.Log(movSpeed); 
         //Debug.Log(isMoving);
     }
@@ -161,19 +230,6 @@ public class CameraMovement : MonoBehaviour
 
     private void Movement()
     {
-        // Wait until camera has rotated to face the direction of the next point before moving
-        //if (transform.rotation == rotationTargetMove)
-        //{
-        //    // Move until you reach the current object/waypoint, and then stop moving
-        //    if (transform.position != movementPoints[selectedMovPoint].position)
-        //    {
-        //        movSpeed = saveMovSpeed;
-
-        //        Vector3 pos = Vector3.MoveTowards(transform.position, movementPoints[selectedMovPoint].position, movSpeed * Time.deltaTime);
-        //        rigbody.MovePosition(pos);
-        //    }
-        //}
-
         // Move until you reach the current object/waypoint, and then stop moving
         if (transform.position != movementPoints[selectedMovPoint].position)
         {
@@ -200,6 +256,7 @@ public class CameraMovement : MonoBehaviour
     public void PressRight()
     {
         selectedMovPoint = (selectedMovPoint + 1) % movementPoints.Length;
+        //selectedRotPoint = (selectedRotPoint + 1) % rotationPoints.Length;
 
         isMoving = true;
 
@@ -209,6 +266,7 @@ public class CameraMovement : MonoBehaviour
     public void PressLeft()
     {
         selectedMovPoint = (selectedMovPoint - 1) % movementPoints.Length;
+        //selectedRotPoint = (selectedRotPoint - 1) % rotationPoints.Length;
 
         isMoving = true;
 
