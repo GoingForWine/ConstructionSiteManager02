5a99734e3378a3de6b6116c006e2ba63d66491eb
SeanDarrah <72384918+SeanDarrah@users.noreply.github.com>
2020-12-14 23:11:51 +0000

    Adding Laptop UI
    **STILL A WORK IN PROGRESS**
    
    - Laptop UI now displays a different screen.
    
    - Interactable Buttons which respond when pressed
    
    - Opening laptop animation working, tried my hardest getting the close animation working, but it's not playing along
    
    - Other screens for button presses have not been added yet
    
    - Seriously.. I spent most of my time trying to get the close animation to go.. it won't effing go.

---
+++ Assets/Scripts/quitScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quitScript : MonoBehaviour
+{
+    // Serializing Fields to show in the Script tab inside Unity
+    [SerializeField]
+    private Animator quitAnimController;
+    [SerializeField]
+    private Button laptopOpen;
+    [SerializeField]
+    private Button laptopQuit;
+
+    void Start()
+    {
+        // Setting the fields to only take in certain assets inside of unity, and respond to their functions
+        quitAnimController = GetComponent<Animator>();
+        Button openButton = laptopOpen.GetComponent<Button>();
+        Button quitButton = laptopQuit.GetComponent<Button>();
+        // Giving each button a method to run when pressed, switching a bool from to true to false
+        quitButton.onClick.AddListener(quit);
+        openButton.onClick.AddListener(open);
+    }
+
+    // Setting the bools inside the Animation Controller parameter to change to 'true' depending on which button has been pressed
+    void open()
+    {
+        quitAnimController.SetBool("openBool", true);
+        quitAnimController.SetBool("quitBool", false);
+    }
+    void quit()
+    {
+        quitAnimController.SetBool("quitBool", true);
+        quitAnimController.SetBool("openBool", false);
+    }
+}
