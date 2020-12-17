d2f6d83fdd5317bbc8197bcc06123cb0225a6685
ArchmageDragon <48251401+ArchmageDragon@users.noreply.github.com>
2020-12-17 08:28:42 +0000

    TypewriterDialogue
    Added the typewriter dialogue, and attempted to add in the crossfade transition
    
    Added interactions to the dog and placeholder character - to be refined once people are here

---
+++ Assets/Scripts/DialogueTesting/Multilevel Dialogue/DialogueManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    public Text NameText;
+    public Text dialogueText;
+    public float delay = 0.01f;
+
+    public Animator dialogueAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialogueAnimator.SetBool("IsOpen", true);
+
+        NameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }    
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(delay);
+            //yield return null;
+
+            //Use either yield return null or yield return new waitforseconds - null is faster, so depends on preference
+        }
+    }
+
+    public void EndDialogue()
+    {
+        dialogueAnimator.SetBool("IsOpen", false);
+    }
+}
---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 2f;
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LevelLoad(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadLastLevel()
+    {
+        StartCoroutine(LevelLoad(SceneManager.GetActiveScene().buildIndex - 1));
+    }
+
+    IEnumerator LevelLoad(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
