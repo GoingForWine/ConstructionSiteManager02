cb79e2d7b7d50fe1192da57b6d1b54ab332eca0c
Nathan K <38962677+NathanK239486@users.noreply.github.com>
2020-12-17 01:14:29 +0000

    'Laptop UI - sean' into 'MERGE_CONFLICT_TEST' (#7)
    * Adding Laptop UI
    
    **STILL A WORK IN PROGRESS**
    
    - Laptop UI now displays a different screen.
    
    - Interactable Buttons which respond when pressed
    
    - Opening laptop animation working, tried my hardest getting the close animation working, but it's not playing along
    
    - Other screens for button presses have not been added yet
    
    - Seriously.. I spent most of my time trying to get the close animation to go.. it won't effing go.
    
    * Safe Commit & Push
    
    Safe Commit & Push
    
    * Laptop UI Done
    
    Will properly describe when merging into main master
    
    Co-authored-by: SeanDarrah <72384918+SeanDarrah@users.noreply.github.com>

---
+++ Assets/Scripts/timeFunction.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timeFunction : MonoBehaviour
+{
+    // Initialising ints and making the clock attachable inside Unity
+    private int hour = 22;
+    private int minute = 30;
+    [SerializeField]
+    private Text clock;
+
+    // creating an unseen timer and couroutine
+    private float timer = 0;
+
+    void Start()
+    {
+        StartCoroutine(incremental());
+        Animation clkAnim = clock.GetComponent<Animation>();
+    }
+
+    // Enum stays true all the time so that we can count seconds instead of using ++
+    // Waits one second, then follows with the IncrementTimer method
+    IEnumerator incremental()
+    {
+        while (true)
+        {
+            //Wait for 1 second
+            yield return new WaitForSeconds(1);
+
+            //Increment
+            incrementTimer();
+        }
+    }
+
+    // IncrementTimer becomes active, adds 1 to the timer
+    void incrementTimer()
+    {
+        timer += 1;
+    }
+
+
+    // if Timer reaches X seconds, then it will follow a series of rules
+    // all the rules follow the way a digital clock works
+    void Update()
+    {
+        if (timer == 1)
+        {
+            minute += 5;
+            timer = 0;
+            if (minute > 55)
+            {
+                hour += 1;
+                minute = 0;
+                timer = 0;
+                if (hour > 23)
+                {
+                    hour = 0;
+                    minute = 0;
+                    timer = 0;
+                }
+            }
+        }
+        // Making it so only a UI Text can be attached
+        Text clk = clock.GetComponent<Text>();
+        // Constantly updating time and setting the ToString to "00" so that 0's can show up in front of numbers
+        clk.text = (hour.ToString("00") + ":" + minute.ToString("00"));
+    }
+}
