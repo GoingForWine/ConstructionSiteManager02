934287859b913ec349b574b1a23fc4538eddaa39
SeanDarrah <72384918+SeanDarrah@users.noreply.github.com>
2021-02-15 20:29:17 +0000

    Relationship functions changed
    The old system worked on an individual slider for each and every worker that was shown on screen on a scale of 0 - 100.
    
    After feedback, it was decided relationships would be better not shown, but interpreted by the player. so we now have two sliders, Good & Bad, tracking every decision made inside the game.
    
    Very Good = +2 GoodMeter
    Good = +1 GoodMeter
    Very Bad = +2 BadMeter
    Bad = +1 BadMeter
    
    This way we can use the slider values to track good and bad decisions which can be used for scripted events and/or end of game results.

---
+++ Assets/Scripts/Editor/relationshipFunctions_EDITOR.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof (relationshipFunctions))]
+public class relationshipFunctions_EDITOR : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/relationshipFunctions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class relationshipFunctions : MonoBehaviour
+{
+    [SerializeField]
+    private Slider GoodMeter;
+    [SerializeField]
+    private Slider BadMeter;
+
+    public choiceEnum ChoiceOutcome = new choiceEnum();
+
+    public enum choiceEnum
+    {
+        VeryGood,
+        Good,
+        VeryBad,
+        Bad,
+        None
+    }
+
+    public void MeterChanges()
+    {
+        if (ChoiceOutcome == choiceEnum.VeryGood)
+        {
+            GoodMeter.value += 2;
+            Debug.Log("Recognising 'Very Good' Setting");
+            ChoiceOutcome = choiceEnum.None;
+        }
+
+        if (ChoiceOutcome == choiceEnum.Good)
+        {
+            GoodMeter.value += 1;
+            Debug.Log("Recognising 'Good' Setting");
+            ChoiceOutcome = choiceEnum.None;
+        }
+
+        if (ChoiceOutcome == choiceEnum.VeryBad)
+        {
+            BadMeter.value += 2;
+            Debug.Log("Recognising 'Very Bad' Setting");
+            ChoiceOutcome = choiceEnum.None;
+        }
+
+        if (ChoiceOutcome == choiceEnum.Bad)
+        {
+            BadMeter.value += 1;
+            Debug.Log("Recognising 'Bad' Setting");
+            ChoiceOutcome = choiceEnum.None;
+        }
+    }
+}
