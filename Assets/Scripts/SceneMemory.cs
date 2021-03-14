12aa0d477e9f7495a13519690d7687af1b9dad37
SeanDarrah <72384918+SeanDarrah@users.noreply.github.com>
2021-03-14 19:50:02 +0000

    Relationship Memory Implemented
    - Relationship Counters now track from scene to scene
    - Commented thoroughly through code and did some general clean up
    - Added a "move to test scene" button so that you can see the implemented changes.
    - SceneMemory code should work universally, but if not, I will look into further again.

---
+++ Assets/Scripts/SceneMemory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMemory : MonoBehaviour
+{
+    // Floats created to take in the values from the relationshipFunctions script
+    public float GoodMeterNumber;
+    public float BadMeterNumber;
+
+    public static SceneMemory Instance;
+
+    // This 'awake' function is called when the above static is loaded
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            // Anything inside of the relationshipFunctions script which refers to the SceneMemory Instance will not be destroyed
+            // This lets us remember all the values that the player has made before, giving us memory.
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            // This is just if the game is closed (I'm guessing though)
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/relationshipFunctions.cs
+++ Assets/Scripts/relationshipFunctions.cs
@@ -8,15 +8,22 @@ using UnityEngine.UI;
 
 public class relationshipFunctions : MonoBehaviour
 {
+    // Serializing the sliders inside of Unity
     [SerializeField]
     private Slider GoodMeter;
     [SerializeField]
     private Slider BadMeter;
+    // Serializing the sliders Text components inside of Unity (tracks the slider value)
     [SerializeField]
     private TextMeshProUGUI GoodMeterNum;
     [SerializeField]
     private TextMeshProUGUI BadMeterNum;
 
+    // Creating a float so that the number can be tracked inside the script
+    public float GoodMeterFlo = 0;
+    public float BadMeterFlo = 0;
+
+    // enum created to give designers an easier time game designing
     public choiceEnum ChoiceOutcome = new choiceEnum();
 
     public enum choiceEnum
@@ -28,36 +35,66 @@ public class relationshipFunctions : MonoBehaviour
         None
     }
 
+    void Start()
+    {
+        // Script floats will always equal the slider value inside of Unity
+        GoodMeterFlo = GoodMeter.value;
+        BadMeterFlo = BadMeter.value;
+
+        // The slider value inside unity will always equal to a float inside of another script (SceneMemory)
+        // This is so values are remembered from scene to scene
+        GoodMeter.value = SceneMemory.Instance.GoodMeterNumber;
+        BadMeter.value = SceneMemory.Instance.BadMeterNumber;
+    }
+
     public void MeterChanges()
     {
+        // Collection of if statements stating which each of the choice enums will process
         if (ChoiceOutcome == choiceEnum.VeryGood)
         {
-            GoodMeter.value += 2;
+            GoodMeterFlo += 2;
             Debug.Log("Recognising 'Very Good' Setting");
-            ChoiceOutcome = choiceEnum.None;
         }
 
         if (ChoiceOutcome == choiceEnum.Good)
         {
-            GoodMeter.value += 1;
+            GoodMeterFlo += 1;
             Debug.Log("Recognising 'Good' Setting");
-            ChoiceOutcome = choiceEnum.None;
         }
 
         if (ChoiceOutcome == choiceEnum.VeryBad)
         {
-            BadMeter.value += 2;
+            BadMeterFlo += 2;
             Debug.Log("Recognising 'Very Bad' Setting");
-            ChoiceOutcome = choiceEnum.None;
         }
 
         if (ChoiceOutcome == choiceEnum.Bad)
         {
-            BadMeter.value += 1;
+            BadMeterFlo += 1;
             Debug.Log("Recognising 'Bad' Setting");
-            ChoiceOutcome = choiceEnum.None;
         }
 
+        // Choice Enum being set to None after If statements are done
+        // This makes it so the number added to the meters is not looped through
+        ChoiceOutcome = choiceEnum.None;
+
+        // Current slider value set to change the script float depending which option is chosen
+        GoodMeter.value = GoodMeterFlo;
+        BadMeter.value = BadMeterFlo;
+
+        // SceneMemory float equaliing to the script float that was set above
+        SceneMemory.Instance.GoodMeterNumber = GoodMeterFlo;
+        SceneMemory.Instance.BadMeterNumber = BadMeterFlo;
+
+        // The slider number changing to equal to the slider value found on the top left of the screen
+        GoodMeterNum.text = GoodMeter.value.ToString();
+        BadMeterNum.text = BadMeter.value.ToString();
+    }
+
+    void Update()
+    {
+        // This keeps the number on screen when changing scenes
+        // Without this the numbers shown are '0', even though the actual values are remembered
         GoodMeterNum.text = GoodMeter.value.ToString();
         BadMeterNum.text = BadMeter.value.ToString();
     }
