889c934fbcb1e8319947ba9d48abed20208945c1
SeanDarrah <72384918+SeanDarrah@users.noreply.github.com>
2021-03-24 20:19:56 +0000

    Commit for joining myself and Chris' branch
    yeye very epic

--- Assets/Scripts/InteractTimeFrame.cs
+++ Assets/Scripts/InteractTimeFrame.cs
@@ -11,10 +11,10 @@ public class InteractTimeFrame : MonoBehaviour
     [SerializeField]
     private BoxCollider characterBoxCollider;
     [SerializeField]
-    private GameObject HeadNotification;
+    public GameObject HeadNotification;
 
     // Creating a bool for if the Notification is active or not yet
-    private bool NotifBool;
+    public bool NotifBool;
 
     //Referencing the current time inside the timeFunction script
     public timeFunction tf = new timeFunction();
